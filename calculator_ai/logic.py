ee7513fe8ed13b8ebdc0ddd772fd04746ea2a6e1
Rishika <rishika@threatsenseai.com>
2025-08-25 16:32:35 +0530

    removed negitive support (#4)
    * removed negitive support
    
    * resolving format
    
    * updated test
    
    ---------
    
    Co-authored-by: ToggleNow <togglenow@ToggleNows-MacBook-Pro.local>

--- calculator_ai/logic.py
+++ calculator_ai/logic.py
@@ -4,7 +4,18 @@ Number = Union[int, float]
 
 
 def add(a: Number, b: Number) -> float:
-    """Return a + b as float."""
+    """Return a + b as float.
+
+    Arg:
+        a: number a
+        b: number b
+
+    Returns:
+        sum of a and b"""
+
+    if float(a) < 0 or float(b) < 0:
+        raise ValueError("Negative inputs not allowed")
+
     return round(float(a) + float(b), 2)
 
 
